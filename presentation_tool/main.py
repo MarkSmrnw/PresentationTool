4e872a0fe94840ce6b0ce3e57c85ec986ec0795f
Mark <mark100smirnow@gmail.com>
2026-03-19 17:33:30 +0100

    Add laser toggle state and button

--- presentation_tool/main.py
+++ presentation_tool/main.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 class AppState: # Nicer way of storing values?
     is_presenting: bool = False
     current_slide: int = 0
+    laser_active: bool = False
 
 class PresentationApp:
     def __init__(self, page: ft.Page):
@@ -39,12 +40,20 @@ class PresentationApp:
             on_click=self.toggle_presentation,
         )
 
+        laser_btn = ft.Button(
+            "Toggle Laser Pointer",
+            on_click=self.toggle_laser,
+            width=300,
+            height=50,
+        )
+
         self.page.add(
             ft.Column(
                 [   # List of things that will be added to the Colum, can be anything ?
                     header,
                     status_text,
                     start_btn,
+                    laser_btn,
                     ft.Divider(height=40),
                     ft.Text("Features coming: Laser, Notes, Clicker...")
                 ],
@@ -57,11 +66,24 @@ class PresentationApp:
 
         self.status_text = status_text
 
+    def toggle_laser(self, e: ft.ControlEvent):
+        self.state.laser_active = not self.state.laser_active
+        self.status_text.value = (
+            f"Status: {'Presenting' if self.state.is_presenting else 'Idle'} | "
+            f"Slide: {self.state.current_slide} | "
+            f"Laser: {'ON' if self.state.laser_active else 'OFF'}"
+        )
+
+        self.page.update()
+
     def toggle_presentation(self, e: ft.ControlEvent):
         self.state.is_presenting = not self.state.is_presenting
         self.status_text.value = (
-            f"Status: {'Presenting' if self.state.is_presenting else 'Idle'} | Slide: {self.state.current_slide}"
+            f"Status: {'Presenting' if self.state.is_presenting else 'Idle'} | "
+            f"Slide: {self.state.current_slide} | "
+            f"Laser: {'ON' if self.state.laser_active else 'OFF'}"
         )
+
         self.page.update()
 
 def main(page: ft.Page):
